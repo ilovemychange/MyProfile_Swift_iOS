9ae266173db1ac8094ccfac1b573db6a27db95e2
Jaemin Ko <ilovemychange@gmail.com>
2019-10-18 21:51:24 +0900

    first commit

---
+++ MyProfile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyProfile
+//
+//  Created by Jaemin Ko on 13/09/2019.
+//  Copyright © 2019 Jaemin Ko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
